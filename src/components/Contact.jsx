1a8910a37df830cf6865b04cbf1c815a8e7f75d1
priyank8141 <jainpriyank789@gmail.com>
2023-03-16 14:53:38 +0530

    fix: send mail update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,52 +11,26 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    // Simple POST request with a JSON body using fetch
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        name: "priyank",
-        email: "jainpriyank789@gamil.com",
-      }),
-    };
-    fetch(
-      "https://public.herotofu.com/v1/2429bc70-ba6d-11ed-ab97-31ebb4a00cbf",
-      requestOptions
-    )
-      .then((response) => {
-        toast.success("Email send Successfully");
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({ postId: data.id });
-        form.current.reset();
-      })
-      .catch((err) => {
-        console.log("err", err);
-        toast.error(err);
-      });
-
-    // emailjs
-    //   .sendForm(
-    //     "YOUR_SERVICE_ID",
-    //     "YOUR_TEMPLATE_ID",
-    //     form.current,
-    //     "YOUR_PUBLIC_KEY"
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //       // Clear all input field values
-    //       form.current.reset();
-    //       // Success toast message
-    //       toast.success("Email send Successfully");
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //       toast.error(error.text);
-    //     }
-    //   );
+    emailjs
+      .sendForm(
+        "service_x2cnpx6",
+        "template_x1vo9si",
+        form.current,
+        "xWI3PcEsgWbJWce5i"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          // Clear all input field values
+          form.current.reset();
+          // Success toast message
+          toast.success("Email send Successfully");
+        },
+        (error) => {
+          console.log(error.text);
+          toast.error(error.text);
+        }
+      );
   };
 
   return (
@@ -73,16 +47,13 @@ const Contact = () => {
         <div className="flex gap-10 md:flex-row flex-col">
           <form
             ref={form}
-            // onSubmit={sendEmail}
+            onSubmit={sendEmail}
             data-aos="fade-up"
             className="flex-1 flex flex-col gap-5"
-            action="https://public.herotofu.com/v1/2429bc70-ba6d-11ed-ab97-31ebb4a00cbf"
-            method="post"
-            accept-charset="UTF-8"
           >
             {/* Input Name as same as email js templates values */}
             <input
-              name="Name"
+              name="to_name"
               id="name"
               type="text"
               placeholder="Name"
@@ -90,7 +61,7 @@ const Contact = () => {
               className="border border-slate-600 p-3 rounded"
             />
             <input
-              name="Email"
+              name="from_name"
               id="email"
               type="email"
               pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
@@ -98,12 +69,12 @@ const Contact = () => {
               required
               className="border border-slate-600 p-3 rounded"
             />
-            {/* <textarea
+            <textarea
               name="message"
               placeholder="Message"
               className="border border-slate-600 p-3 rounded h-44"
               required
-            ></textarea> */}
+            ></textarea>
             <button
               className="btn self-start
             bg-white text-dark_primary"
